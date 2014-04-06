88efe657e18847c5ffb0732c9bdcba836d84186e
Charimon <acharkin@gmail.com>
2014-04-06 00:28:36 -0700

    added more files

---
+++ CharStatusBarView/CharAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CharAppDelegate.h
+//  CharStatusBarView
+//
+//  Created by Andrew Charkin on 4/5/14.
+//  Copyright (c) 2014 Charimon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CharAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CharStatusBarView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CharStatusBarView
+//
+//  Created by Andrew Charkin on 4/5/14.
+//  Copyright (c) 2014 Charimon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CharAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CharAppDelegate class]));
+    }
+}
